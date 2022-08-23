3be6719b1a3db57c6f5f59c7993606f1d16c3f99
Sangay Thinley <thinley.sangay@selise.ch>
2022-08-23 18:53:41 +0600

    refactor(flutter_app): enable the assets path in pubspec.yaml file

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Flutter Demo',
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Demo'),
     );
   }
 }
